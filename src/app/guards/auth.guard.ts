47c58d41edf7fd847b7cfb0076b1df333314160f
Lio10jr <kevinzambrano593@gmail.com>
2025-01-21 16:24:08 -0500

    Guards para usuario autenticado y no autenticado

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../services/user.service';
-import { switchMap } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const userServ = inject(UserService);
