23f97d13c6f41e53a55eeaa606a5832535543e55
Lio10jr <kevinzambrano593@gmail.com>
2025-02-15 15:17:06 -0500

    feat: Nueva ruta para actualizar un evento.

--- src/app/features/events/routes.ts
+++ src/app/features/events/routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { PrincipalComponent } from "./components/principal/principal.component";
 import { authGuard } from "../../core/guards";
 import { CreateEventComponent } from "./components/create-event/create-event.component";
+import { EditEventComponent } from "./components/edit-event/edit-event.component";
 
 
 export const eventsRoutes: Routes = [
     { path: '', component: PrincipalComponent, canActivate: [authGuard] },
     { path: 'create', component: CreateEventComponent, canActivate: [authGuard] },
+    { path: 'edit', component: EditEventComponent, canActivate: [authGuard] },
 ]
