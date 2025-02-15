3649b4ba4b4e820ffc35dc9a400b2d4aa81f8679
Lio10jr <kevinzambrano593@gmail.com>
2025-02-15 12:44:03 -0500

    feat: Ajuste del la posición de los componentes mostrados debajo del header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { initFlowbite } from 'flowbite';
+
 @Component({
   selector: 'app-root',
   imports: [HeaderComponent, RouterOutlet],
-  template:  ` 
+  template:  `
     <app-header /> 
-    <router-outlet />
+    <div class="content">
+      <router-outlet />
+    </div>
   `,
-  styles: [],
+  styles: [`
+    .content {
+      margin-top: 4rem; 
+      height: calc(100vh - 4rem);
+      overflow: auto;
+    }
+  `],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bingo_fronted';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
