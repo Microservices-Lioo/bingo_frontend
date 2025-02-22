2d3490e796a71f992838ab938a3a411e55995546
Lio10jr <kevinzambrano593@gmail.com>
2025-02-22 18:22:13 -0500

    feat(Routing): Nuevas rutas para home para ver todos los eventos, ver todos los eventos por estado.

--- src/app/features/home/routes.ts
+++ src/app/features/home/routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./components/principal/home.component";
+import { EventsListComponent } from "./components/events-list/events-list.component";
+import { HomeSidebarComponent } from "./home-sidebar/home-sidebar.component";
 
 export const homeRoutes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', redirectTo: 'home/principal', pathMatch: 'full'},
+    { path: '', component: HomeSidebarComponent, 
+        children: [
+            { path: 'principal', component: HomeComponent },
+            { path: 'all-events', component: EventsListComponent },
+            { path: '', redirectTo: 'principal', pathMatch: 'full'}
+        ]
+    },
 ];
