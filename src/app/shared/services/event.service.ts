ff39a43b2ec24d32f9c8385c8c8ab6f1776f8f72
Lio10jr <kevinzambrano593@gmail.com>
2025-02-22 18:25:01 -0500

    feat(EventService): Actualización del servicio de eventos compartido para una interface que retorna un atributo buyer adicional como booleano.

--- src/app/shared/services/event.service.ts
+++ src/app/shared/services/event.service.ts
@@ -6,23 +6,27 @@ import { environment } from '../../../environments/environment';
 import { handleError } from '../../core/errors';
 import { StatusEvent } from '../enums';
 
+export interface EventWithBuyerInterface extends EventInterface {
+  buyer?: boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
-export class EventService {
+export class EventServiceShared {
   url: string = environment.apiUrl + environment.apiMSEventUrl;
 
   constructor(
     private http: HttpClient
   ) { }
 
-  eventListStatus(status: StatusEvent, pagination: { limit?: number, page?: number }): Observable<PaginationInterface<EventInterface>> {
-    return this.http.get<PaginationInterface<EventInterface>>(`${this.url}/status/${status}`, { params: pagination })
+  eventListStatus(status: StatusEvent, pagination: { limit?: number, page?: number }): Observable<PaginationInterface<EventWithBuyerInterface>> {
+    return this.http.get<PaginationInterface<EventWithBuyerInterface>>(`${this.url}/status/${status}`, { params: pagination })
       .pipe(catchError(handleError));
   }
 
-  eventListByUserStatus(status: StatusEvent, pagination: { limit?: number, page?: number }): Observable<PaginationInterface<EventInterface>> {
-    return this.http.get<PaginationInterface<EventInterface>>(`${this.url}/by-user/status/${status}`, { params: pagination })
+  eventListByUserStatus(status: StatusEvent, pagination: { limit?: number, page?: number }): Observable<PaginationInterface<EventWithBuyerInterface>> {
+    return this.http.get<PaginationInterface<EventWithBuyerInterface>>(`${this.url}/by-user/status/${status}`, { params: pagination })
       .pipe(catchError(handleError));
   }
 }
