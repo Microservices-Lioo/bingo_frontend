fb2d3d1b2df974aa66d8e7b3db04964bad787c46
Lio10jr <kevinzambrano593@gmail.com>
2025-02-23 18:12:45 -0500

    feat(Routes): Nueva ruta view para ver la información de un evento y validación para rutas no encontradas.

--- src/app/features/events/routes.ts
+++ src/app/features/events/routes.ts
@@ -3,10 +3,14 @@ import { PrincipalComponent } from "./components/principal/principal.component";
 import { authGuard } from "../../core/guards";
 import { CreateEventComponent } from "./components/create-event/create-event.component";
 import { EditEventComponent } from "./components/edit-event/edit-event.component";
+import { ViewEventComponent } from "./components/view-event/view-event.component";
 
 
 export const eventsRoutes: Routes = [
-    { path: '', component: PrincipalComponent, canActivate: [authGuard] },
+    { path: 'principal', component: PrincipalComponent, canActivate: [authGuard] },
     { path: 'create', component: CreateEventComponent, canActivate: [authGuard] },
     { path: 'edit', component: EditEventComponent, canActivate: [authGuard] },
+    { path: 'view', component: ViewEventComponent },
+    { path: '', redirectTo: '/principal', pathMatch: 'full'},
+    { path: '**', component: PrincipalComponent }
 ]
