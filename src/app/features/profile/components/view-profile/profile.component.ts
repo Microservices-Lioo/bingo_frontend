4e6a775af80b07eb60947c4ba56d223493a00f20
Lio10jr <kevinzambrano593@gmail.com>
2025-02-27 17:22:41 -0500

    feat(Profile): Componente dinamico para ver ver el perfil de un usuario.

--- src/app/features/profile/components/view-profile/profile.component.ts
+++ src/app/features/profile/components/view-profile/profile.component.ts
@@ -1,6 +1,10 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { PrimaryButtonComponent } from '../../../../ui/buttons/primary-button/primary-button.component';
 import { UserInterface } from '../../../../core/interfaces';
+import { LoadingService, ToastService } from '../../../../shared/services';
+import { UserService } from '../../services';
+import { AuthService } from '../../../auth/services';
 
 @Component({
   selector: 'app-profile',
@@ -11,11 +15,56 @@ import { UserInterface } from '../../../../core/interfaces';
 })
 export class ProfileComponent implements OnInit {
   user: UserInterface | any = null;
+  isLogged: boolean = false;
+
+  constructor(
+    private loadingServ: LoadingService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private userServ: UserService,
+    private toastServ: ToastService,
+    private authServ: AuthService
+  ) {}
 
   ngOnInit() {
-    const userData = localStorage.getItem('user');
-    if (userData) {
-      this.user = JSON.parse(userData);
+    this.loadingServ.loadingOn();
+    const id = this.route.snapshot.paramMap.get('id');
+    if (!id) {      
+      this.user = this.authServ.currentUser;
+      this.authServ.isLoggedIn$.subscribe( (logged) => {
+        this.isLogged = logged;
+      });
+      this.loadingServ.loadingOff();
+    } else {
+      this.getUserOne(+id);
     }
-  }  
+  }
+
+  getUserOne(id: number) {
+    this.userServ.getUser(id).subscribe({
+      next: (user) => {
+        if (user) {
+          this.user = user;
+          this.loadingServ.loadingOff();
+        } else {
+          this.router.navigate(['/home/principal']);
+          this.toastServ.openToast('user', 'danger', 'El usuario no existe');
+          this.loadingServ.loadingOff();
+        }
+      },
+      error: (error) => {
+        this.router.navigate(['/home/principal']);
+        this.toastServ.openToast('user', 'danger', 'No se encontro los datos del usuario');
+        this.loadingServ.loadingOff();
+      }
+    })
+  }
+
+  getLetter(): string {
+    const arrayLetterName = this.user.name.split('');
+    const arrayLetterLastname = this.user.lastname.split('');
+    return arrayLetterName[0] + arrayLetterLastname[0];
+  }
+
+
 }
