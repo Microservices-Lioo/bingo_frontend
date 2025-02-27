7ead3c554168399ef2d31d0adc771eed4d84a941
Lio10jr <kevinzambrano593@gmail.com>
2025-02-27 17:21:35 -0500

    feat(HomeSidebar): Integranción de LoadingIndicatorComponent en el componente principal del home.

--- src/app/features/home/home-sidebar/home-sidebar.component.ts
+++ src/app/features/home/home-sidebar/home-sidebar.component.ts
@@ -1,15 +1,17 @@
 import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { LoadingIndicatorComponent } from '../../../shared/components/loading-indicator/loading-indicator.component';
 
 @Component({
   selector: 'app-home-sidebar',
-  imports: [SidebarComponent, RouterOutlet],
+  imports: [SidebarComponent, RouterOutlet, LoadingIndicatorComponent],
   template: `
     <!-- Sidebar -->
     <app-sidebar />
     <div class="p-4 sm:ml-64">
       <router-outlet />
+      <app-loading-indicator />
     </div>
   `,
   styles: ``
