1d76ca29e4652878bc2e94c35ab2755557259a27
Lio10jr <kevinzambrano593@gmail.com>
2025-05-24 01:55:33 -0500

    feat(PrincipalComponent): Diseño del estado de los premios del juego de bingo.

--- src/app/features/games/components/principal.component.ts
+++ src/app/features/games/components/principal.component.ts
@@ -6,6 +6,16 @@ import { BallStatusComponent } from './ball-status/ball-status.component';
 import { initFlowbite } from 'flowbite';
 import { GamesService } from '../services/games.service';
 
+
+
+type AwardType = {
+  num: number;
+  name: string;
+  winner: string;
+  winner_status: boolean;
+  status: boolean;
+};
+
 @Component({
   selector: 'app-principal',
   imports: [BallStatusComponent],
@@ -29,21 +39,55 @@ export class PrincipalComponent implements OnInit {
     [true, false, false, false, true],
   ];
   numCalled: number | null = null;
-  
+  awardsList: AwardType[];
+
   constructor(
     private route: ActivatedRoute,
     private eventSharedServ: EventServiceShared,
     private gameServ: GamesService
   ) {
+    this.awardsList = this.getAwardsList();
   }
 
   ngOnInit() {
     const eventId = this.route.snapshot.paramMap.get('id');
     const userId = this.route.snapshot.paramMap.get('userId');
     this.getEvent(+eventId!, +userId!);
+
     // this.calledBall(5);
     // this.cleanBalls(true);
     initFlowbite();
+  }
+
+  getAwardsList(): AwardType[] {
+    let awards = [
+      { num: 1, name: 'Premio 1', winner: 'Kevin', winner_status: true, status: false },
+      { num: 2, name: 'Premio 2', winner: '', winner_status: false, status: true },
+      { num: 3, name: 'Premio 3', winner: '', winner_status: false, status: false },
+      { num: 4, name: 'Premio 4', winner: '', winner_status: false, status: false },
+      { num: 5, name: 'Premio 5', winner: '', winner_status: false, status: false},
+      { num: 6, name: 'Premio 6', winner: '', winner_status: false, status: false },
+    ];
+
+    return this.orderAwards(awards);
+  }
+
+  orderAwards(awards: AwardType[]) {    
+    let auxActualxList: AwardType;
+    let auxFinList: AwardType[] = [];
+    let auxProxList: AwardType[] = [];
+
+    awards.forEach(element => {
+      if (element.status) {
+        auxActualxList = element;
+      } else if (element.winner_status) {
+        auxFinList.push(element);
+      } else {
+        auxProxList.push(element);
+      }
+    });
+    let awardsResult = [auxActualxList!, ...auxProxList.concat(...auxFinList)];
+    return awardsResult;
   }
 
   getEvent(eventId: number, userId: number) {
